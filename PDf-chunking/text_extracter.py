19b1dce6644c7fa2746a0a671fc49e6db8732add
Talha <talhafareed0101@gmail.com>
2026-08-27 01:13:16 +0500

    gitignore Updated.

--- PDf-chunking/text_extracter.py
+++ PDf-chunking/text_extracter.py
@@ -1,13 +1,18 @@
 import pdfplumber as pf
-def extract_text(file):
-    text =""
-    with pf.open(file) as f:
-        for page in f.pages:
-            page_text = page.extract_text()
-            if page_text:
-                text += page_text + "\n" 
-    return text
 
+def extract_text(file):
+    "Takes a PDF file as input and returns the extracted text from all pages."
+    try:
+        text =""
+        with pf.open(file) as f:
+            for page in f.pages:
+                page_text = page.extract_text()
+                if page_text:
+                    text += page_text + "\n" 
+        return text
+    except Exception as e:
+        print(f"Error extracting text from PDF: {e}")
+        raise
 
 
         
